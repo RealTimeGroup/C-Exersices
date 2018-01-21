365518c7bbd52afadc332e45cdfa8ad8e876a1c8
RealTimeGroup <35651988+RealTimeGroup@users.noreply.github.com>
2018-01-21 10:56:38 +0200

    findfactorial C file
    C code to find the factorial of a given number

---
+++ findfactorial.c
@@ -0,0 +1,37 @@
+/*
+ *  Find factorial of number
+ *  Author:Real Time Group
+ *  All Rights Reserved
+ */
+
+# include <stdio.h>
+
+//declare a function that find the factorial of the number
+int factorial (int num);
+
+//============================
+int main()
+{
+    	int Num1, tmp;
+
+    	printf("ENTER the Nuber its factorial you want : ");
+    	scanf("%d",&Num1);
+    	tmp=Num1;
+    	printf(" **************************************** \n ");
+    	printf(" The factorial of %d is %d \n",tmp,factorial(Num1));
+   
+  
+	return 0;
+    
+}
+//==========================
+int factorial(int num){
+	int  retfactorial=1;
+	while (num>0)
+    	{
+        	retfactorial *= num;
+        	num = num - 1;
+    	}
+    	return retfactorial;
+  
+}
